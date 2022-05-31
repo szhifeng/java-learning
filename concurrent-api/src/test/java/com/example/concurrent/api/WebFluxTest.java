efdff6fcb982cb9582887a5abe025cc09f4f8081
szf <15775021238@163.com>
2022-05-31 14:29:27 +0800

    polishing

---
+++ concurrent-api/src/test/java/com/example/concurrent/api/WebFluxTest.java
@@ -0,0 +1,10 @@
+package com.example.concurrent.api;
+
+/**
+ * @author szf
+ * @describe: DOTO
+ * @Date 2022/5/31 13:57
+ */
+public class WebFluxTest {
+
+}
---
+++ concurrent-netty/src/test/java/com/example/concurrent/netty/NettyTests.java
@@ -0,0 +1,15 @@
+package com.example.concurrent.netty;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author szf
+ * @describe: netty 测试类
+ * @Date 2022/5/31 13:51
+ *
+ */
+@SpringBootTest
+public class NettyTests {
+
+
+}
